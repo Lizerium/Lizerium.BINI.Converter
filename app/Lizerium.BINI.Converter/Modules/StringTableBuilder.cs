c735322e4b69973e8626a877e1a4ec54970df038
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-25 11:12:43 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-25

--- app/Lizerium.BINI.Converter/Modules/StringTableBuilder.cs
+++ app/Lizerium.BINI.Converter/Modules/StringTableBuilder.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 мая 2026 06:52:17
- * Version: 1.0.46
+ * Last Updated: 25 мая 2026 11:12:39
+ * Version: 1.0.47
  */
 
 using System.Text;
