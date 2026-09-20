441debcd5736ad24b53dfa2430500807389a3ee5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-20 08:42:58 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-20

--- app/Lizerium.BINI.Converter.App/Cli/ConsoleConversionReporter.cs
+++ app/Lizerium.BINI.Converter.App/Cli/ConsoleConversionReporter.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 сентября 2026 08:45:44
- * Version: 1.0.165
+ * Last Updated: 20 сентября 2026 08:42:54
+ * Version: 1.0.166
  */
 
 using Lizerium.BINI.Converter.App.Conversion;
